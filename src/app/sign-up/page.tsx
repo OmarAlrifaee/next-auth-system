e8de5492d305fee6a475b583f3509f634db7dd56
OmarAlrifaee <jivara211@gmail.com>
2024-05-27 18:31:42 +0300

    hundle the sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,22 +1,47 @@
+"use client";
 import Link from "next/link";
 import axios from "axios";
-type User = {
-  email: string;
-  password: string;
-  username: string;
-};
-const onSignUpAction = async (data: FormData) => {
-  "use server";
-  const { email, password, username } = Object.fromEntries(data);
-};
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 const SignUp = () => {
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+  // functions
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.post("/api/users/sign-up", {
+        username: user.username,
+        email: user.email,
+        password: user.password,
+      });
+      if (data) {
+        setError(false);
+        router.replace("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <section className="min-h-screen flex justify-center items-center">
       <form
-        action={onSignUpAction}
+        // action={onSignUpAction}
         className="flex flex-col items-center gap-8"
+        onSubmit={onSubmit}
       >
         <h1 className="text-6xl font-bold">Signup</h1>
+        {error ? <span>something went wronge please try agine</span> : ""}
         <div className="flex items-center gap-3 relative">
           <label htmlFor="username" className="absolute top-[-24px]">
             username:
@@ -27,6 +52,9 @@ const SignUp = () => {
             id="username"
             name="username"
             placeholder="username"
+            required
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
           />
         </div>
         <div className="flex items-center gap-3  relative">
@@ -39,6 +67,9 @@ const SignUp = () => {
             id="email"
             name="email"
             placeholder="email"
+            required
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
         </div>
         <div className="flex items-center gap-3 relative">
@@ -51,11 +82,15 @@ const SignUp = () => {
             id="password"
             name="password"
             placeholder="password"
+            required
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </div>
         <button
           type="submit"
           className="bg-blue-500 text-white px-5 py-2 font-bold"
+          disabled={loading}
         >
           signup
         </button>
